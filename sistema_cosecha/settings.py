959702cbbffc8b7d8d838bc63ba5f49d09dae7db
Bastian <bastian.araneda1@virginiogomez.cl>
2026-05-11 22:35:13 -0400

    Implementando seguridad con variables de entorno

--- sistema_cosecha/settings.py
+++ sistema_cosecha/settings.py
@@ -75,14 +75,17 @@ WSGI_APPLICATION = 'sistema_cosecha.wsgi.application'
 
 
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cosecha_db',
-        'USER': 'admin',
-        'PASSWORD': 'Cosecha2026-',
-        'HOST': 'cosecha-db.cxuq2y0qed2z.us-east-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
